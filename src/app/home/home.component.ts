a9786354eaf3c13ce8f93c83b349f80ed8a6b041
zaturnvalley <zacharycmason@gmail.com>
2018-03-15 19:15:05 -0700

    added interpolated values

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4;
+  btnText: string = 'Add an item';
+  
   constructor() { }
 
   ngOnInit() {
