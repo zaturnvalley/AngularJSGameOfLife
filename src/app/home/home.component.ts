9ae253197abdd43fb0d4dbf6ac817a8898634aab
zaturnvalley <zacharycmason@gmail.com>
2018-03-16 09:50:48 -0700

    added add item method, added itemCount to ngOnInit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,13 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  itemCount: number = 4;
+  itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
+  goals = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem(){
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
   }
 
 }
