ef0dad2b14f85073d203893b129aa4c60e814879
zaturnvalley <zacharycmason@gmail.com>
2018-03-16 19:39:53 -0700

    added trigger animations to goals to ease-in

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,22 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+
+    trigger('goals', [
+      transition('* => *', [
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1})
+          ]))]), {optional: true})
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
