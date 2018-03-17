5bd6e8e9e3042d4473b682e62952abfc4091e5e3
zaturnvalley <zacharycmason@gmail.com>
2018-03-17 12:36:19 -0700

    added removeItem method and leave animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,13 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
             style({opacity: 1, transform: 'translateY(0)', offset: 1})
-          ]))]), {optional: true})
+          ]))]), {optional: true}),
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
+          ]))]), {optional: true})        
       ])
     ])
   ]
@@ -38,6 +44,10 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 
 }
