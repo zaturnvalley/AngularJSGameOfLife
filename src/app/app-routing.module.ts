c3f4ab12b724c64f062312003b9f956ce679a4ef
zaturnvalley <zacharycmason@gmail.com>
2018-03-19 10:10:18 -0700

    added routes for home about game comps

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { GameComponent } from './game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'game',
+    component: GameComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
