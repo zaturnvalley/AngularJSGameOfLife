3bd644125967efe03a78f2bd12c6891f86d19ef8
zaturnvalley <zacharycmason@gmail.com>
2018-03-19 16:46:58 -0700

    fixed syntax error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -38,8 +38,8 @@ export class HomeComponent implements OnInit {
   constructor(private _data: DataService) { }
 
   ngOnInit() {
-    this.itemCount = this.goals.length;
-    this._data.subscribe(res => this.goals = res);
+    this._data.goal.subscribe(res => this.goals = res);
+    this.itemCount = this.goals.length;    
     this._data.changeGoal(this.goals);
   }
 
